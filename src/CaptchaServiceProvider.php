a85a036d8741144ff945e65435b1c22eac3f59fc
Aryeh Raber <aryeh.raber@gmail.com>
2020-08-21 19:45:06 +0200

    Remove boot method due to auto-publishing & merging

--- src/CaptchaServiceProvider.php
+++ src/CaptchaServiceProvider.php
@@ -22,23 +22,6 @@ class CaptchaServiceProvider extends AddonServiceProvider
         'web' => __DIR__.'/../routes/web.php',
     ];
 
-    public function boot()
-    {
-        parent::boot();
-
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'captcha');
-
-        $this->mergeConfigFrom(__DIR__.'/../config/captcha.php', 'captcha');
-
-        $this->publishes([
-            __DIR__.'/../config/captcha.php' => config_path('captcha.php'),
-        ], 'config');
-
-        $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/captcha'),
-        ], 'views');
-    }
-
     public function register()
     {
         $this->app->bind(Captcha::class, function() {
